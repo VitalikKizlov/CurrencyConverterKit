2d457f005de1057be4b6905a6ef4c88a61d2c51f
Vitalii Kizlov <vitalii kizlov>
2023-02-19 12:58:01 +0100

    Add more tests

--- Tests/TransferTests/CountriesStoreServiceTests.swift
+++ Tests/TransferTests/CountriesStoreServiceTests.swift
@@ -8,15 +8,15 @@
 import XCTest
 @testable import Transfer
 
-public final class CountriesStoreServiceTests: XCTestCase {
+final class CountriesStoreServiceTests: XCTestCase {
 
     @Injected(\.countriesStoreService) private var countriesStoreService: CountriesStoreProtocol
 
-    override func setUpWithError() throws {
+    public override func setUpWithError() throws {
         try super.setUpWithError()
     }
 
-    override func tearDownWithError() throws {
+    public override func tearDownWithError() throws {
         try super.tearDownWithError()
     }
 
@@ -31,6 +31,45 @@ public final class CountriesStoreServiceTests: XCTestCase {
         let contains = countries.contains(where: { $0 == .poland })
 
         XCTAssertFalse(contains)
+    }
+
+    func testCountriesForReceiver() {
+        let countries = countriesStoreService.countriesForReceiver()
+
+        XCTAssertNotNil(countries)
+    }
+
+    func testCountriesForSenderDoesntContainReceiverCountry() {
+        let countries = countriesStoreService.countriesForReceiver()
+        let contains = countries.contains(where: { $0 == .ukraine })
+
+        XCTAssertFalse(contains)
+    }
+
+    func testSenderCountry() {
+        let country = countriesStoreService.senderCountry()
+
+        XCTAssertEqual(country, .poland)
+    }
+
+    func testReceiverCountry() {
+        let country = countriesStoreService.receiverCountry()
+
+        XCTAssertEqual(country, .ukraine)
+    }
+
+    func testSenderCountryConfigurationOnSelection() {
+        countriesStoreService.configureSenderCountry(for: "Germany")
+        let country = countriesStoreService.senderCountry()
+
+        XCTAssertEqual(country, .germany)
+    }
+
+    func testReceiverCountryConfigurationOnSelection() {
+        countriesStoreService.configureReceiverCountry(for: "Great Britain")
+        let country = countriesStoreService.receiverCountry()
+
+        XCTAssertEqual(country, .greatBritain)
     }
 
 }
